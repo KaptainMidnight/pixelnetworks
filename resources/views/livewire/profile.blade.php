02ec41673dd92687eb7330a49333a5aa069c2196
Александр Артемкин <aleksandrartemkin@MacBook-Air-Aleksandr.local>
2020-11-29 23:15:54 +0400

    Bug fix

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -7,7 +7,7 @@
     <div class="rounded rounded-t-lg overflow-hidden shadow max-w-xs my-3 mx-auto">
         <img src="https://miro.medium.com/max/3800/1*CkLmBVwhdFESTKW4VVkJkA.jpeg" class="w-full"/>
         <div class="flex justify-center -mt-8">
-            @if(Auth::id() === 151)
+            @if($user->id === 151)
                 <img src="https://media2.giphy.com/media/13PMpiayBvKFck/giphy.gif?cid=ecf05e47ijo4v4n7odhz15t5vhbrqy0glmttfktimuoloy96&amp;rid=giphy.gifds" class="rounded-full border-solid border-white border-2 -mt-3">
             @else
                 <img src="http://via.placeholder.com/150" class="rounded-full border-solid border-white border-2 -mt-3">
